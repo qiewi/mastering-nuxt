735bc47eb9d11029a26f79e2fdc30484a8c42e13
Rizqi Andhika <qiewifruit@gmail.com>
2025-06-26 17:33:42 +0700

    chore: vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt'],
   css: ['~/.nuxt/tailwind.css'],
   tailwindcss: {
     cssPath: '~/.nuxt/tailwind.css'
