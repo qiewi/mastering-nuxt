107732eac5a0619c3991dad6c2f45a8da98be689
Rizqi Andhika <qiewifruit@gmail.com>
2025-06-26 12:39:45 +0700

    feat: course page

--- pages/course.vue
+++ pages/course.vue
@@ -1,1 +1,32 @@
-<template>this a course page</template>
+<template>
+    <div
+        class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center"
+    >
+        <div class="prose mb-12">
+            <h1>
+                <span class="font-medium">
+                    Course:
+                    <span class="font-bold">Mastering Nuxt 3</span>
+                </span>
+            </h1>
+        </div>
+
+        <div class="flex flex-row justify-center flex-grow">
+            <div
+                class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col"
+            >
+                <h3 class="font-bold">Chapters</h3>
+                <!-- Lesson and course will be listed here -->
+            </div>
+
+            <div class="prose p-12 bg-white rounded-md w-[65ch]">
+                <h2 class="font-bold">Lessons</h2>
+                <p>This is a lesson</p>
+                <!-- Lesson and course will be listed here -->
+            </div>
+        </div>
+    </div>
+
+    
+
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,5 @@
-<template>Hello, Mastering Nuxt 3</template>
+<template>
+  <div class="min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 flex items-center justify-center">
+    <h1 class="text-4xl font-bold text-white shadow-lg">Hello, Mastering Nuxt 3</h1>
+  </div>
+</template>
