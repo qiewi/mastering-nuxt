c8f82223db45349ca49ccd2bce11860adc2bf4bb
Rizqi Andhika <qiewifruit@gmail.com>
2025-06-26 15:14:59 +0700

    feat: sourceUrl

--- composables/courseData.js
+++ composables/courseData.js
@@ -10,6 +10,7 @@ export default {
             title: 'Introduction to TypeScript with Vue.js 3',
             slug: '1-introduction-to-typescript-with-vue-js-3',
             number: 1,
+            sourceUrl: '',
             downloadUrl: 'https://vueschool.io/lessons/introduction-to-typescript-with-vue-js-3',
             videoId: 684236333,
             text: `In this lesson, we take a look at some of the benefits of using TypeScript in Vue.js applications:
@@ -24,6 +25,7 @@ export default {
             title: 'TypeScript in Vue Components',
             slug: '2-typescript-in-vue-components',
             number: 2,
+            sourceUrl: '',
             downloadUrl: 'https://vueschool.io/lessons/typescript-in-vue-components',
             videoId: 684236334,
             text: `Learn how to properly set up TypeScript in Vue.js 3 components using the Composition API.`
@@ -39,6 +41,7 @@ export default {
             title: 'Advanced TypeScript Patterns',
             slug: '1-advanced-typescript-patterns',
             number: 1,
+            sourceUrl: '',
             downloadUrl: 'https://vueschool.io/lessons/advanced-typescript-patterns',
             videoId: 684236335,
             text: `Explore advanced TypeScript patterns and how to apply them in Vue.js applications.`
@@ -47,6 +50,7 @@ export default {
             title: 'TypeScript with Pinia',
             slug: '2-typescript-with-pinia',
             number: 2,
+            sourceUrl: '',
             downloadUrl: 'https://vueschool.io/lessons/typescript-with-pinia',
             videoId: 684236336,
             text: `Learn how to use TypeScript with Pinia for type-safe state management.`
