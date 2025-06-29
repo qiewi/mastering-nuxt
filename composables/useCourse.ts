8699c7922f6216efcb6ba74224c087273f86c6fb
Rizqi Andhika <qiewifruit@gmail.com>
2025-06-29 09:21:50 +0700

    refactor: useCourse types

--- composables/useCourse.ts
+++ composables/useCourse.ts
@@ -1,14 +1,43 @@
 import courseData from './courseData'
 
-export default function useCourse() {
+type Lesson = {
+  title: string;
+  slug: string;
+  number: number;
+  downloadUrl: string;
+  videoId: number;
+  text: string;
+  sourceUrl?: string;
+}
+
+type Chapter = {
+  title: string;
+  slug: string;
+  number: number;
+  lessons: Lesson[];
+}
+
+type Course = {
+  title: string;
+  chapters: Chapter[];
+}
+
+
+export const useCourse = (): Course => {
+  const chapters: Chapter[] = courseData.chapters.map(
+    (chapter) => {
+    const lessons: Lesson[] = chapter.lessons.map(
+      (lesson) => ({
+      ...lesson,
+      path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
+    }));
+    return { 
+      ...chapter, 
+      lessons 
+    };
+  });
   return {
     ...courseData,
-    chapters: courseData.chapters.map((chapter) => ({
-      ...chapter,
-      lessons: chapter.lessons.map((lesson) => ({
-        ...lesson,
-        path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
-      })),
-    })),
+    chapters,
   }
 }
