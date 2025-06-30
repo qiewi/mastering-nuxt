1bdad1e39b6fc930f4a4fa27a0e6c2ff7d545d81
Rizqi Andhika <qiewifruit@gmail.com>
2025-06-30 22:56:20 +0700

    feat: github login

--- pages/login.vue
+++ pages/login.vue
@@ -5,6 +5,7 @@
         </h1>
         <button
             class="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+            @click="login"
         >
             Log in with Github
         </button>
@@ -13,4 +14,30 @@
 
 <script setup lang="ts">
 const { title } = useCourse();
+const supabase = useSupabaseClient();
+const user = useSupabaseUser();
+
+// Redirect to home if already logged in
+if (user.value) {
+    await navigateTo('/');
+}
+
+watch(user, (newUser) => {
+    if (newUser) {
+        navigateTo('/');
+    }
+});
+
+const login = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({
+        provider: 'github',
+        options: {
+            redirectTo: `${window.location.origin}/login`
+        }
+    });
+
+    if (error) {
+        console.error(error);
+    }
+};
 </script>
