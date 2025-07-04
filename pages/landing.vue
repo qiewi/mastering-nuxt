6daf61cfd0813a5c0520dab4c963f08622b0090f
Rizqi Andhika <qiewifruit@gmail.com>
2025-07-04 16:50:49 +0700

    feat: preview section

--- pages/landing.vue
+++ pages/landing.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="space-y-12 flex flex-col items-center py-16 px-8 bg-white">
-    <h1 class="text-7xl font-black text-blue-500 m-0 py-16">
+    <h1 class="text-7xl font-black text-blue-500 m-0 pb-16">
       {{ course.title }}
     </h1>
     <img
@@ -8,7 +8,9 @@
       class="w-full rounded-lg shadow-lg border-2 border-slate-200"
     />
     <div class="text-2xl font-medium">
-      Learn how to use TypeScript in your Vue projects to supercharge your IDE's error detection and autocompletion, as well as provide type safe code that's easier to reason about and refactor.
+      Learn how to use TypeScript in your Vue projects to supercharge your IDE's
+      error detection and autocompletion, as well as provide type safe code
+      that's easier to reason about and refactor.
     </div>
     <button
       class="bg-yellow-400 hover:bg-yellow-500 transition px-8 py-4 w-[40ch]"
@@ -31,7 +33,50 @@
     </ul>
   </Section>
 
-  <Section title="Course Outline" variant="white">
+  <Section title="Course Preview" variant="white">
+    <div class="container mx-auto px-4 py-16">
+      <div class="grid grid-cols-1 lg:grid-cols-2 gap-8">
+        <div
+          class="overflow-hidden rounded-lg shadow-lg transform hover:scale-105 hover:-rotate-2 transition-transform duration-300"
+        >
+          <img
+            :src="screenshots[0]"
+            alt="Screenshot 1"
+            class="w-full h-full object-cover"
+          />
+        </div>
+        <div
+          class="overflow-hidden rounded-lg shadow-lg transform hover:scale-105 hover:rotate-2 transition-transform duration-300"
+        >
+          <img
+            :src="screenshots[0]"
+            alt="Screenshot 2"
+            class="w-full h-full object-cover"
+          />
+        </div>
+        <div
+          class="overflow-hidden rounded-lg shadow-lg transform hover:scale-105 hover:-rotate-2 transition-transform duration-300"
+        >
+          <img
+            :src="screenshots[0]"
+            alt="Screenshot 3"
+            class="w-full h-full object-cover"
+          />
+        </div>
+        <div
+          class="overflow-hidden rounded-lg shadow-lg transform hover:scale-105 hover:rotate-2 transition-transform duration-300"
+        >
+          <img
+            :src="screenshots[0]"
+            alt="Screenshot 4"
+            class="w-full h-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  </Section>
+
+  <Section title="Course Outline" variant="blue">
     <ul class="text-2xl font-medium space-y-16">
       <li
         v-for="(chapter, index) in course.chapters"
@@ -54,9 +99,7 @@
             <Badge color="bg-blue-400">
               {{ lesson.number }}
             </Badge>
-            <span class="text-xl opacity-80">{{
-              lesson.title
-            }}</span>
+            <span class="text-xl opacity-80">{{ lesson.title }}</span>
           </li>
         </ul>
       </li>
@@ -68,18 +111,16 @@
 </template>
 
 <script setup lang="ts">
-import CourseScreen from '~/assets/images/course.png';
+import CourseScreen from "~/assets/images/course.png";
 const course = await useCourse();
 const learningOutcomes = [
-  'Hands-On Experience with the Benefits of TypeScript',
-  'How to make the most out of your IDE',
-  'How to use TypeScript with the Composition API',
-  'How to use TypeScript with the Options API',
-  'How to type reactive data, refs, props, custom events, event handlers, DOM elements, template refs, data from provide/inject, and more'
+  "Hands-On Experience with the Benefits of TypeScript",
+  "How to make the most out of your IDE",
+  "How to use TypeScript with the Composition API",
+  "How to use TypeScript with the Options API",
+  "How to type reactive data, refs, props, custom events, event handlers, DOM elements, template refs, data from provide/inject, and more",
 ];
-const screenshots = [
-  CourseScreen,
-];
+const screenshots = [CourseScreen];
 
 definePageMeta({
   layout: false,
